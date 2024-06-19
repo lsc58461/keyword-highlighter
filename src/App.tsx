0650e2ef4b25a04eeb4033fb1dcc152e44754e75
lsc58461 <lsc58461@naver.com>
2024-06-20 03:22:43 +0900

    feat: 검색 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchPage from "./page/SearchPage";
+
 function App() {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-56pxr font-bold">
-      hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,16 +23,16 @@ function SearchBar({ onSearchValueChange }: ISearchBarProps) {
   }, [searchValue, onSearchValueChange]);
 
   return (
-    <form className="relative" onSubmit={handleSubmit}>
+    <form className="relative w-full" onSubmit={handleSubmit}>
       <input
-        className="peer w-420pxr rounded-2xl bg-gray-100 py-12pxr pl-12pxr pr-100pxr text-22pxr font-bold text-gray-700 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400 active:scale-100 active:bg-gray-300"
+        className="peer w-full rounded-2xl bg-gray-100 py-12pxr pl-12pxr pr-85pxr text-22pxr font-bold text-gray-700 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400 active:scale-100 active:bg-gray-300 tablet:pr-100pxr"
         type="search"
         title="search bar"
         placeholder="검색을 해보세요!"
         onChange={handleValueChange}
       />
       <button
-        className="absolute right-3pxr top-1/2 w-82pxr -translate-y-1/2 transform rounded-2xl bg-cyan-400 px-12pxr py-8pxr text-22pxr font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-[1.16] hover:bg-cyan-500 active:scale-100 active:bg-cyan-600 peer-hover:translate-x-8pxr peer-hover:scale-105 peer-active:-translate-x-1pxr peer-active:scale-100"
+        className="absolute right-4pxr top-[49.9%] w-82pxr -translate-y-1/2 transform rounded-[14px] bg-cyan-400 px-12pxr py-8pxr text-22pxr font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:-translate-x-[2.5px] hover:scale-[1.16] hover:bg-cyan-500 active:translate-x-1pxr active:scale-100 active:bg-cyan-600 peer-hover:translate-x-19pxr peer-hover:scale-105 peer-active:translate-x-0pxr peer-active:scale-100"
         type="submit"
       >
         검색
